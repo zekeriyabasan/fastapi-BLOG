f75ee937543a28b43fed2f632c869de98ceae689
zekeriyabasan <zekeriyabasan@gmail.com>
2026-06-14 13:51:14 +0300

    CORS config middleware

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from database.database import engine
 from database import models
 from routers import delete, get, post
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(post.router)
@@ -12,3 +13,15 @@ app.include_router(delete.router)
 models.Base.metadata.create_all(engine)
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
+
+origins =[
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
